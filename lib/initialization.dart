7345fe5f39c62f27847497f46aa394b446a8a3e0
priyanshuverma-dev <priyanshuverma@outlook.in>
2024-06-06 14:34:14 +0530

    nav side bar

--- lib/initialization.dart
+++ lib/initialization.dart
@@ -1,5 +1,6 @@
 import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
 import 'package:system_theme/system_theme.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -13,6 +14,12 @@ Future<void> initialiseAppFunctions() async {
 
 Future<void> initWindowManager() async {
   await windowManager.ensureInitialized();
+  await Window.initialize();
+
+  await Window.setEffect(
+    effect: WindowEffect.acrylic,
+    color: const Color(0xCC222222),
+  );
 
   WindowOptions windowOptions = const WindowOptions(
     center: true,
